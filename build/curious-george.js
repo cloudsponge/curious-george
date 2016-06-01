8fa625f2c912fced1e18da6bfe5d040be48933a3
abozhilov <asen.bozhilov@gmail.com>
2016-06-01 11:32:50 +0300

    Initial commit

---
+++ build/curious-george.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var DNS_API = 'http://dig.jsondns.org/IN/tlrobinson.net/A';
+
+var CuriousGeorge = function CuriousGeorge() {
+    _classCallCheck(this, CuriousGeorge);
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const DNS_API = 'http://dig.jsondns.org/IN/tlrobinson.net/A'
+
+class CuriousGeorge {
+    
+}

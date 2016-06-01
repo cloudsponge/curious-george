4076aecc173ea8b415dc409d10c9d15ac6256b66
abozhilov <asen.bozhilov@gmail.com>
2016-06-01 15:40:33 +0300

    Initial API

--- build/curious-george.js
+++ build/curious-george.js
@@ -1,9 +1,85 @@
 'use strict';
 
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var DNS_API = 'http://dig.jsondns.org/IN/tlrobinson.net/A';
-
-var CuriousGeorge = function CuriousGeorge() {
-    _classCallCheck(this, CuriousGeorge);
+var GLOBAL = window;
+var ERROR_MSG = {
+    INVALID_EMAIL: 'Invalid email address',
+    UNREACHABLE_API: 'API is unreachable'
 };
+
+var CuriousGeorge = (function () {
+    function CuriousGeorge() {
+        _classCallCheck(this, CuriousGeorge);
+    }
+
+    _createClass(CuriousGeorge, [{
+        key: 'lookup',
+        value: function lookup(email) {
+            var host = this.getHostName(email);
+            return new Promise(function (resolve, reject) {
+                if (host) {
+                    (function () {
+                        var id = CuriousGeorge.getId();
+                        GLOBAL['CuriousGeorge__jsonp__' + id] = function (obj) {
+                            if (!obj) {
+                                reject(ERROR_MSG.UNREACHABLE_API);
+                            } else {
+                                resolve(obj);
+                            }
+                            CuriousGeorge.cleanup(id);
+                        };
+                        CuriousGeorge.injectScript(id, host);
+                    })();
+                } else {
+                    reject(ERROR_MSG.INVALID_EMAIL);
+                }
+            });
+        }
+    }, {
+        key: 'getHostName',
+        value: function getHostName(email) {
+            var match = /@(.+)$/.exec(email);
+
+            if (match) {
+                return match[1];
+            }
+            return null;
+        }
+    }], [{
+        key: 'injectScript',
+        value: function injectScript(id, host) {
+            var script = document.createElement('script');
+            var callback = 'CuriousGeorge__jsonp__' + id;
+
+            script.id = 'script_' + id;
+            script.async = true;
+            script.onerror = GLOBAL[callback];
+            script.src = 'http://dig.jsondns.org/IN/' + host + '/MX?callback=' + callback;
+
+            document.head.appendChild(script);
+        }
+    }, {
+        key: 'cleanup',
+        value: function cleanup(id) {
+            var script = document.getElementById('script_' + id);
+
+            GLOBAL['CuriousGeorge__jsonp__' + id] = null;
+            script.parentNode.removeChild(script);
+        }
+    }, {
+        key: 'getId',
+        value: function getId() {
+            if (!CuriousGeorge.__id__) {
+                CuriousGeorge.__id__ = 0;
+            }
+            return ++CuriousGeorge.__id__;
+        }
+    }]);
+
+    return CuriousGeorge;
+})();
+
+//# sourceMappingURL=curious-george.js.map
--- src/index.js
+++ src/index.js
@@ -1,5 +1,66 @@
-const DNS_API = 'http://dig.jsondns.org/IN/tlrobinson.net/A'
+const GLOBAL = window;
+const ERROR_MSG = {
+    INVALID_EMAIL : 'Invalid email address',
+    UNREACHABLE_API : 'API is unreachable'
+}
 
 class CuriousGeorge {
     
+    lookup(email) {
+        let host = this.getHostName(email);
+        return new Promise((resolve, reject) => {
+            if (host) {
+                let id = CuriousGeorge.getId();                
+                GLOBAL[`CuriousGeorge__jsonp__${id}`] = (obj) => {
+                    if (!obj) {
+                        reject(ERROR_MSG.UNREACHABLE_API);
+                    }
+                    else {
+                        resolve(obj);
+                    }
+                    CuriousGeorge.cleanup(id);
+                }
+                CuriousGeorge.injectScript(id, host);
+            }
+            else {
+                reject(ERROR_MSG.INVALID_EMAIL);
+            }
+        })
+    }
+    
+    getHostName(email) {
+        let match = /@(.+)$/.exec(email)
+        
+        if (match) {
+            return match[1];
+        }
+        return null
+    }
+    
+    static injectScript(id, host) {
+        let script = document.createElement('script');
+        let callback = `CuriousGeorge__jsonp__${id}`;
+        
+        script.id = `script_${id}`;
+        script.async = true;
+        script.onerror = GLOBAL[callback];
+        script.src = `http://dig.jsondns.org/IN/${host}/MX?callback=${callback}`
+        
+        document.head.appendChild(script);
+    }
+    
+    static cleanup(id) {
+        var script = document.getElementById(`script_${id}`);
+        
+        GLOBAL[`CuriousGeorge__jsonp__${id}`] = null;
+        script.parentNode.removeChild(script);
+    }
+    
+    static getId() {
+        if (!CuriousGeorge.__id__) {
+            CuriousGeorge.__id__ = 0;
+        }
+        return ++CuriousGeorge.__id__;
+    }
 }
+
